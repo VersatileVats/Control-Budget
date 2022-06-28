87946fd31fbc82b7a6de1511600b8d361c8356c7
VATS <57909173+VersatileVats@users.noreply.github.com>
2022-06-28 15:18:35 +0530

    Update plan_details_script.php

--- utilities/plan_details_script.php
+++ utilities/plan_details_script.php
@@ -11,16 +11,13 @@
    
     // Using POST method to extract the details
     $title = $_POST['title'];
-    $title = mysqli_real_escape_string($connect, $title);
-    $start = $_POST['start'];
-    $end = $_POST['end'];
 
     // Query to update / insert the data into the plans table
     $update_query = "UPDATE plans SET title= '$title', start= '$start', end = '$end' WHERE id = '$last';";
     $result_update_query = mysqli_query($connect, $update_query) or die(mysqli_error($connect));
 
     // Query to insert the user_plans table
-    $insert_query = "INSERT INTO user_plans (u_id, p_id) VALUES ('$_SESSION[id]','$last')";
+    $insert_query = "INSERT INTO user_plans (u_id) VALUES ('$_SESSION[id]')";
     $result_insert_query = mysqli_query($connect,$insert_query) or die(mysqli_error($connect));
 
     header('location: ./../home.php');
